3f34a4efe5421558c84a10c9bb921c314c0d8a26
Liispet-Tess Talu <liispet-tesstalu@LiispetTesssMBP.lan>
2023-12-18 19:04:53 +0200

    create new app

---
+++ FileReading/Program.cs
@@ -0,0 +1,19 @@
+﻿//andmete lugemine failist macis
+
+namespace DataFromFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"/Users/liispet-tesstalu/projects/data/heroes.txt";
+
+            string [] dataFromFile = File.ReadAllLines(filePath);
+
+            foreach(string element in dataFromFile)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
